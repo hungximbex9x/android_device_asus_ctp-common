cb2d30035e001faf82602d17c34cb2777a14b5c0
tank0412 <romabukhtiarov@gmail.com>
2016-09-26 18:35:17 +0300

    ctp-common: fix power and camera driver building

--- power/Android.mk
+++ power/Android.mk
@@ -17,8 +17,7 @@ LOCAL_PATH := $(call my-dir)
 # HAL module implemenation stored in
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
-
-LOCAL_CFLAGS += -Werror
+#LOCAL_CFLAGS += -Werror
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
 LOCAL_SRC_FILES := power.cpp
